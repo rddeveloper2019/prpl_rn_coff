f41ab699ff0f77478d573a2d6ed37d57bced9e24
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2025-08-16 16:31:24 +0300

    5.9

--- index.tsx
+++ index.tsx
@@ -1,43 +1,76 @@
 import { registerRootComponent } from "expo";
 import React from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import {
+  Dimensions,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { Colors, FontSize, Gaps } from "./shared/tokens";
+import { Button } from "./shared/Button";
 
 function App() {
+  const height = Dimensions.get("screen").height;
   return (
-    <View style={styles.container}>
-      <View style={styles.content}>
-        <View style={styles.intro}>
-          <Text style={styles.title}>Одно из самых вкусных кофе в городе!</Text>
-          <Text style={styles.description}>
-            Свежие зёрна, настоящая арабика и бережная обжарка
-          </Text>
-        </View>
-        <Button title="Начать" />
+    <>
+      <View style={styles.container}>
+        <ImageBackground
+          source={require("./assets/bg.png")}
+          style={{ ...styles.image, height: height * 0.8 }}
+          resizeMode="cover"
+        >
+          <View style={styles.content}>
+            <View style={styles.intro}>
+              <Text style={styles.title}>
+                Одно из самых вкусных кофе в городе!
+              </Text>
+              <Text style={styles.description}>
+                Свежие зёрна, настоящая арабика и бережная обжарка
+              </Text>
+            </View>
+            <Button text="Начать" />
+          </View>
+        </ImageBackground>
       </View>
-    </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "flex-end",
-    paddingHorizontal: 30,
-    paddingVertical: 45,
-    backgroundColor: "#000000",
+    // justifyContent: "flex-start",
+    paddingBottom: 55,
+    backgroundColor: Colors.Black100,
+    height: "100%",
   },
-  content: { alignItems: "stretch", justifyContent: "center", gap: 24 },
-  intro: { alignItems: "center", justifyContent: "center", gap: 8 },
+  content: {
+    alignItems: "stretch",
+    justifyContent: "flex-end",
+    gap: Gaps._24,
+    height: "100%",
+    paddingHorizontal: 30,
+  },
+  intro: { alignItems: "center", justifyContent: "center", gap: Gaps._8 },
   title: {
     fontWeight: 600,
-    fontSize: 34,
+    fontSize: FontSize._34,
     lineHeight: 43,
     textAlign: "center",
     letterSpacing: 0.01,
-    color: "#FFFFFF",
+    color: Colors.White,
   },
-  description: { color: "#A9A9A9", textAlign: "center", paddingHorizontal: 30 },
+  description: {
+    color: Colors.Grey,
+    textAlign: "center",
+    paddingHorizontal: 30,
+    fontSize: FontSize._14,
+  },
+  image: {
+    flex: 1,
+    justifyContent: "flex-end",
+  },
 });
 
 registerRootComponent(App);
