df2ea3d628cc17143310844a94848c1e22df652b
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2025-08-22 22:58:21 +0300

    add icon

--- widgets/SearchPanel.tsx
+++ widgets/SearchPanel.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, TextInput, View, ViewStyle } from 'react-native';
 import { Colors, FontSize, Radius } from '../shared/tokens';
+import Svg, { Path } from 'react-native-svg';
 
 export const SearchPanel = ({
   containerStyles,
@@ -11,24 +12,48 @@ export const SearchPanel = ({
 }) => {
   return (
     <View style={{ ...styles.container, ...containerStyles }}>
-      <TextInput
-        style={styles.input}
-        placeholder="Найти кофе"
-        placeholderTextColor={Colors.GreyText}
-      />
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Найти кофе"
+          placeholderTextColor={Colors.GreyText}
+        />
+        <Svg
+          width={20}
+          height={20}
+          viewBox="0 0 20 20"
+          fill="none"
+          style={styles.icon}
+        >
+          <Path
+            d="M9.583 17.5a7.917 7.917 0 100-15.833 7.917 7.917 0 000 15.833zM18.333 18.333l-1.666-1.666"
+            stroke="#fff"
+            strokeWidth={1.5}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </Svg>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: { backgroundColor: Colors.Black100 },
-  input: {
+  inputContainer: {
+    position: 'relative',
     backgroundColor: Colors.BlackLight,
     margin: 30,
     height: 52,
     borderRadius: Radius._16,
-    padding: 17,
-    color: Colors.GreyText,
+
     fontSize: FontSize._14,
+  },
+
+  input: { padding: 17, color: Colors.GreyText, paddingLeft: 48 },
+  icon: {
+    position: 'absolute',
+    top: 16,
+    left: 16,
   },
 });
